d18ccebfa59e9104c484fc176dda678c5fb6c629
Abhijith S Nair <202478673+abhijithsnair1111@users.noreply.github.com>
2025-12-31 11:53:15 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -17,7 +17,7 @@ GO
 CREATE TABLE bronze.crm_cust_info(
 	cst_id                INT,
 	cst_key               VARCHAR(50),
-	cst_firatname         VARCHAR(50),
+	cst_firstname         VARCHAR(50),
 	cst_lastname          VARCHAR(50),
 	cst_marital_status    VARCHAR(50),
 	cst_gndr              VARCHAR(50),
